03e2b56bdc32ffe67ef072ae8594d4590b76e41c
jessie-sr <jessie_sr@163.com>
2024-02-11 20:42:12 +0800

    Attempted adding new restaurant feature

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import RestaurantListView, RestaurantDetailView
+from . import views
 
 urlpatterns = [
     path('restaurants/', RestaurantListView.as_view(), name='restaurant-list'),
     path('restaurants/<int:pk>/', RestaurantDetailView.as_view(), name='restaurant-detail'),
+    path('restaurants/add/', views.add_restaurant, name='add_restaurant'),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import ListView
 from django.views.generic import DetailView
+from django.shortcuts import render, redirect
 from .models import Restaurant
+from .forms import RestaurantForm
 
 class RestaurantListView(ListView):
     model = Restaurant
@@ -11,3 +13,13 @@ class RestaurantDetailView(DetailView):
     model = Restaurant
     template_name = 'restaurants/detail.html'
     context_object_name = 'restaurant'
+
+def add_restaurant(request):
+    if request.method == 'POST':
+        form = RestaurantForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('restaurants:list')
+    else:
+        form = RestaurantForm()
+    return render(request, 'restaurants/add.html', {'form': form})
