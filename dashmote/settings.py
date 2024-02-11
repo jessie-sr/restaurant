621ebf24af849d92609d23e6c4226bd00453f85e
jessie-sr <jessie_sr@163.com>
2024-02-11 13:45:16 +0800

    Created Restaurant model and applied migration

--- dashmote/settings.py
+++ dashmote/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restaurants',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'dashmote.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'restaurant_dev_db',
+        'USER': 'jessiesun',
+        'PASSWORD': 'Sr6208H%llo1019',
+        'HOST': 'localhost',  # e.g., 'localhost' or an IP address
+        'PORT': '5432',  # default PostgreSQL port is 5432
     }
 }
 
--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Definition of the models.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    cuisine = models.CharField(max_length=100)
+    rating = models.FloatField()
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
