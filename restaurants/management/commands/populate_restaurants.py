a7cddbfca9286c901804d6c0d7bdab2616b003f1
jessie-sr <jessie_sr@163.com>
2024-02-16 22:12:51 +0800

    Updated populate_restaurants

--- restaurants/management/commands/populate_restaurants.py
+++ restaurants/management/commands/populate_restaurants.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from models import Restaurant  # adjust "your_app" and "Restaurant" to fit your project
+from restaurants.models import Restaurant  # adjust "your_app" and "Restaurant" to fit your project
 import json
 
 class Command(BaseCommand):
