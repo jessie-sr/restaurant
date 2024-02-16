0183fac974fe831093d86a9df8ea2245db789b55
jessie-sr <jessie_sr@163.com>
2024-02-16 21:12:19 +0800

    Set STATIC_ROOT for collectstatic

--- dashmote/settings.py
+++ dashmote/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +134,5 @@ LOGIN_REDIRECT_URL = '/api/restaurants/'
 LOGIN_URL = 'login'
 
 LOGOUT_REDIRECT_URL = '/api/restaurants/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
