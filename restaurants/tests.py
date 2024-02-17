bebbf0dc0a522160fe73a6cda9c18b6acebedc45
jessie-sr <jessie_sr@163.com>
2024-02-17 09:06:35 +0800

    Added test cases

--- restaurants/tests.py
+++ restaurants/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Restaurant
 
-# Create your tests here.
+class RestaurantListViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create test data
+        number_of_restaurants = 5
+        for restaurant_id in range(number_of_restaurants):
+            Restaurant.objects.create(name=f'Restaurant {restaurant_id}', location='Location', cuisine='Cuisine', rating=4.5)
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/api/restaurants/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('restaurant-list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_lists_all_restaurants(self):
+        response = self.client.get(reverse('restaurant-list'))
+        self.assertEqual(len(response.context['restaurants']), 5)
