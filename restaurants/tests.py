470a5eaa339afe66794a317cbd74c7adb7ce3ce3
jessie-sr <jessie_sr@163.com>
2024-02-17 12:33:44 +0800

    Improved test case 3 and 4

--- restaurants/tests.py
+++ restaurants/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from .models import Restaurant
+from django.contrib.auth.models import User
 
 # Test Case 1: Viewing a List of All Restaurants
 class RestaurantListViewTests(TestCase):
@@ -32,4 +33,56 @@ class RestaurantDetailViewTests(TestCase):
         response = self.client.get(reverse('restaurant-detail', args=[self.restaurant.id]))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, self.restaurant.name)
+
+# Test Case 3: Adding a New Restaurant
+class RestaurantCreateViewTests(TestCase):
+    def setUp(self):
+        # Create a test user
+        self.user = User.objects.create_user(username='testuser', password='12345')
+    
+    def test_create_restaurant(self):
+        # Log in the test user
+        self.client.login(username='testuser', password='12345')
+        
+        response = self.client.post(reverse('add_restaurant'), {
+            'name': 'New Restaurant', 
+            'location': 'New Location', 
+            'cuisine': 'New Cuisine', 
+            'rating': 4.0,
+            'phone': 123-456-789,
+            'email': 'info@newrestaurant.com'
+        })
+        
+        self.assertEqual(response.status_code, 302)  # Assuming redirect after creation
+        self.assertEqual(Restaurant.objects.last().name, 'New Restaurant')
+
+# Test Case 4: Updating an Existing Restaurant
+class RestaurantUpdateViewTests(TestCase):
+    def setUp(self):
+        # Create a test user and a test restaurant
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.restaurant = Restaurant.objects.create(
+            name='Old Restaurant', 
+            location='Old Location', 
+            cuisine='Old Cuisine', 
+            rating=3.5,
+            phone=123-456-789,
+            email='info@oldrestaurant.com'
+        )
+
+    def test_update_restaurant(self):
+        # Log in the test user
+        self.client.login(username='testuser', password='12345')
+        
+        self.client.post(reverse('restaurant-update', args=[self.restaurant.id]), {
+            'name': 'Updated Restaurant', 
+            'location': 'Updated Location', 
+            'cuisine': 'Updated Cuisine', 
+            'rating': 4.5,
+            'phone': 123-456-789,
+            'email': 'info@newrestaurant.com'
+        })
+        
+        self.restaurant.refresh_from_db()
+        self.assertEqual(self.restaurant.name, 'Updated Restaurant')
 
